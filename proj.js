a5428fcee13df7822414213766855f922c6d296a
5Knight2 <pratiktarale258@gmail.com>
2023-06-17 15:34:43 +0530

    frontend

--- proj.js
+++ proj.js
@@ -1,11 +1,29 @@
 let form=document.querySelector('#my-form');
 let ul=document.querySelector('#expenses');
-for(let i=0;i<localStorage.length;i++){
-    let obj=JSON.parse(localStorage.getItem(localStorage.key(i)));
-            let str=obj.amount+'-'+obj.description+'-'+obj.type;
-            
+
+form.addEventListener('submit',add);
+ul.addEventListener('click',removelist);
+showall();
+
+function showall(){
+    axios.get("http://localhost:4000/expense")
+    .then((response)=>{console.log(response) 
+     
+        for(let i=0;i<response.data.length;i++){
+        let obj=response.data[i];
+        shownew(obj);
+}
+})
+    .catch((err)=>{console.log(err)})
+}
+
+function shownew(obj){
+  
+    let str=obj.amount+'-'+obj.description+'-'+obj.type;
+    
 let ul=document.querySelector('#expenses');
 let li=document.createElement('li');
+li.id=obj.id;
 let button1=document.createElement('button');
 li.appendChild(document.createTextNode(str));
 let btn=document.createElement('button');
@@ -20,12 +38,12 @@ btn2.className='edit';
 
 li.appendChild(btn2);   
 li.appendChild(btn);
-ul.appendChild(li);  
+ul.appendChild(li); 
 }
 
 
-form.addEventListener('submit',add);
-ul.addEventListener('click',removelist);
+
+
 
 function add(e){
     e.preventDefault();
@@ -38,26 +56,12 @@ function add(e){
         description:desc.value,
         type:typ.value,
     }
-    let serialized=JSON.stringify(obj);
-localStorage.setItem(localStorage.length+1,serialized);
+    axios.post("http://localhost:4000/expense",obj)
+    .then((response)=>{console.log(response) 
+    shownewuser(obj)})
+    .catch((err)=>{console.log(err)})
 
-let ul=document.querySelector('#expenses');
-let li=document.createElement('li');
-let button1=document.createElement('button');
-li.appendChild(document.createTextNode(amt.value+'-'+desc.value+'-'+typ.value));
-let btn=document.createElement('button');
-btn.style.backgroundColor='red';
-btn.appendChild(document.createTextNode('Delete'));
-btn.className='delete';
-
-let btn2=document.createElement('button');
-btn2.style.backgroundColor='gray';
-btn2.appendChild(document.createTextNode('Edit'));
-btn2.className='edit';
-
-li.appendChild(btn2);   
-li.appendChild(btn);
-ul.appendChild(li);  
+shownew(obj);
 
 
 }
@@ -69,32 +73,17 @@ function removelist(e){
         console.log(e.target.parentElement);
         
 
-        for(let i=0;i<localStorage.length;i++){
-            let obj=JSON.parse(localStorage.getItem(localStorage.key(i)));
-            let str=obj.amount+'-'+obj.description+'-'+obj.type;
-            
-            let str2=e.target.parentElement.textContent;
-            str2=str2.substring(0,str2.length-10);
-           
-            if(str==str2){
-                localStorage.removeItem(localStorage.key(i));
-                ul.removeChild(e.target.parentElement);
+        
 
-                console.log(e.target.textContent);
-                if(e.target.textContent=='Edit'){
-                    let amt=document.querySelector('#expense_amount');
-                    let desc=document.querySelector('#expense_desc');
-                    let typ=document.querySelector('#type');
-              
-                amt.value=obj.amount;
-                
-                desc.value=obj.description;
-                
-                typ.value=obj.type;
-                }
-                
-            }
-        }
+        axios.get("http://localhost:4000/expense/"+e.target.parentElement.id)
+        .then((response)=>{console.log(response) 
+         
+            for(let i=0;i<response.data.length;i++){
+            let obj=response.data[i];
+           
+    }
+    })
+        .catch((err)=>{console.log(err)})
 
         
     }
