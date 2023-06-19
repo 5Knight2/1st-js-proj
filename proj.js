0f68aa5121be11efcad2db4f4a39e0a3a038c362
5Knight2 <pratiktarale258@gmail.com>
2023-06-19 11:06:47 +0530

    frontend with delete and edit

--- proj.js
+++ proj.js
@@ -51,17 +51,21 @@ function add(e){
     let amt=document.querySelector('#expense_amount');
     let desc=document.querySelector('#expense_desc');
     let typ=document.querySelector('#type');
+    let id=document.querySelector('#expense_id');
     let obj={
+        id:id.value,
         amount:amt.value,
         description:desc.value,
         type:typ.value,
     }
     axios.post("http://localhost:4000/expense",obj)
     .then((response)=>{console.log(response) 
-    shownewuser(obj)})
+        if(id.value!='')location.reload();
+        else
+    shownew(obj)})
     .catch((err)=>{console.log(err)})
 
-shownew(obj);
+
 
 
 }
@@ -69,22 +73,34 @@ shownew(obj);
 function removelist(e){
     e.preventDefault();
     let ul=document.querySelector('#expenses');
-    if(e.target.classList.contains('delete')|| e.target.classList.contains('edit')){
-        console.log(e.target.parentElement);
-        
-
-        
-
-        axios.get("http://localhost:4000/expense/"+e.target.parentElement.id)
+    if(e.target.classList.contains('delete')){
+        axios.get("http://localhost:4000/delete/"+e.target.parentElement.id)
         .then((response)=>{console.log(response) 
-         
-            for(let i=0;i<response.data.length;i++){
-            let obj=response.data[i];
-           
-    }
+         console.log('hii')
+         ul.removeChild(e.target.parentElement);
+            
     })
         .catch((err)=>{console.log(err)})
 
         
+    }else if(e.target.classList.contains('edit')){
+        let amt=document.querySelector('#expense_amount');
+        let desc=document.querySelector('#expense_desc');
+        let typ=document.querySelector('#type');
+        let id=document.querySelector('#expense_id');
+
+        axios.get("http://localhost:4000/expense/"+e.target.parentElement.id)
+        .then((response)=>{console.log(response) 
+        //show in form
+        amt.value=response.data.amount;
+        typ.value=response.data.type;
+        desc.value=response.data.description;
+        id.value=response.data.id;
+        })
+        .catch((err)=>{console.log(err)})
+            
     }
-}
+
+        
+
+    }
